52f17214b8642c16209fecfcdf0ec0aa76a5471e
Rob <info@definedhosting.co.uk>
2020-07-06 10:14:36 +0100

    update version #

--- dh-localised.php
+++ dh-localised.php
@@ -6,7 +6,7 @@ GitHub Plugin URI: DefinedHosting/dh-localised
 GitHub Plugin URI: https://github.com/DefinedHosting/dh-localised
 Description: Plugin for displaying localised and personalised content based on landing page entry
 Author: R. Cush
-Version: 5.1.6
+Version: 5.1.8.1
 Author URI: https://www.definedhosting.co.uk
 */
 include_once('core.php');
