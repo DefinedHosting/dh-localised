222ffbd382c07c8ea8a524ecd69e2e56b8f9fc91
Rob <info@definedhosting.co.uk>
2020-12-09 14:10:29 +0000

    5.4.6

--- dh-localised.php
+++ dh-localised.php
@@ -4,7 +4,7 @@ Plugin Name: Defined Hosting Localised & Personalised ShortCodes
 Plugin URI: http://www.definedhosting.co.uk/plugins
 Description: Plugin for displaying localised and personalised content based on landing page entry
 Author: R. Cush
-Version: 5.4.5
+Version: 5.4.6
 Author URI: https://www.definedhosting.co.uk
 GitHub Plugin URI: DefinedHosting/dh-localised
 GitHub Plugin URI: https://github.com/DefinedHosting/dh-localised
